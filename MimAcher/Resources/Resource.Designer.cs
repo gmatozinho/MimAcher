c774971769e265c12469571b7f957a55b8ff006e
gmatozinho <gustavomatozinho@gmail.com>
2016-08-25 20:12:26 -0300

    Teste

--- MimAcher/Resources/Resource.Designer.cs
+++ MimAcher/Resources/Resource.Designer.cs
@@ -54,13 +54,16 @@ namespace MimAcher
 			public const int logo = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int MimAcherIconBig = 2130837507;
+			public const int losango = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int round_button = 2130837508;
+			public const int MimAcherIconBig = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int splash = 2130837509;
+			public const int round_button = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int splash = 2130837510;
 			
 			static Drawable()
 			{
@@ -75,80 +78,80 @@ namespace MimAcher
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060012
-			public const int Buttonsline = 2131099666;
+			// aapt resource value: 0x7f060017
+			public const int Buttonsline = 2131099671;
 			
 			// aapt resource value: 0x7f060000
 			public const int Logo = 2131099648;
 			
-			// aapt resource value: 0x7f060009
-			public const int Nome_line = 2131099657;
+			// aapt resource value: 0x7f06000e
+			public const int Nome_line = 2131099662;
 			
-			// aapt resource value: 0x7f060007
-			public const int Senha_line = 2131099655;
+			// aapt resource value: 0x7f06000c
+			public const int Senha_line = 2131099660;
 			
-			// aapt resource value: 0x7f060005
-			public const int Usuario_line = 2131099653;
+			// aapt resource value: 0x7f06000a
+			public const int Usuario_line = 2131099658;
 			
-			// aapt resource value: 0x7f060010
-			public const int avançar = 2131099664;
+			// aapt resource value: 0x7f060015
+			public const int avançar = 2131099669;
 			
 			// aapt resource value: 0x7f060002
 			public const int avançar2 = 2131099650;
 			
-			// aapt resource value: 0x7f060018
-			public const int avançar3 = 2131099672;
+			// aapt resource value: 0x7f060007
+			public const int avançar3 = 2131099655;
 			
-			// aapt resource value: 0x7f06000a
-			public const int email = 2131099658;
+			// aapt resource value: 0x7f06000f
+			public const int email = 2131099663;
 			
-			// aapt resource value: 0x7f06000b
-			public const int email_line = 2131099659;
+			// aapt resource value: 0x7f060010
+			public const int email_line = 2131099664;
 			
-			// aapt resource value: 0x7f060011
-			public const int entrar = 2131099665;
+			// aapt resource value: 0x7f060016
+			public const int entrar = 2131099670;
 			
 			// aapt resource value: 0x7f060001
 			public const int exibirfoto = 2131099649;
 			
-			// aapt resource value: 0x7f06000c
-			public const int idade = 2131099660;
+			// aapt resource value: 0x7f060011
+			public const int idade = 2131099665;
 			
-			// aapt resource value: 0x7f06000d
-			public const int idade_line = 2131099661;
+			// aapt resource value: 0x7f060012
+			public const int idade_line = 2131099666;
 			
-			// aapt resource value: 0x7f060013
-			public const int inscrevase = 2131099667;
-			
-			// aapt resource value: 0x7f060014
-			public const int linearLayout1 = 2131099668;
+			// aapt resource value: 0x7f060018
+			public const int inscrevase = 2131099672;
 			
 			// aapt resource value: 0x7f060003
-			public const int logo = 2131099651;
-			
-			// aapt resource value: 0x7f060015
-			public const int name_user = 2131099669;
-			
-			// aapt resource value: 0x7f060016
-			public const int name_user_line = 2131099670;
+			public const int linearLayout1 = 2131099651;
 			
 			// aapt resource value: 0x7f060008
-			public const int nome = 2131099656;
-			
-			// aapt resource value: 0x7f060017
-			public const int o_que_vc_gosta = 2131099671;
-			
-			// aapt resource value: 0x7f060006
-			public const int senha = 2131099654;
-			
-			// aapt resource value: 0x7f06000e
-			public const int telefone = 2131099662;
-			
-			// aapt resource value: 0x7f06000f
-			public const int telefone_line = 2131099663;
+			public const int logo = 2131099656;
 			
 			// aapt resource value: 0x7f060004
-			public const int usuario = 2131099652;
+			public const int name_user = 2131099652;
+			
+			// aapt resource value: 0x7f060005
+			public const int name_user_line = 2131099653;
+			
+			// aapt resource value: 0x7f06000d
+			public const int nome = 2131099661;
+			
+			// aapt resource value: 0x7f060006
+			public const int o_que_vc_gosta = 2131099654;
+			
+			// aapt resource value: 0x7f06000b
+			public const int senha = 2131099659;
+			
+			// aapt resource value: 0x7f060013
+			public const int telefone = 2131099667;
+			
+			// aapt resource value: 0x7f060014
+			public const int telefone_line = 2131099668;
+			
+			// aapt resource value: 0x7f060009
+			public const int usuario = 2131099657;
 			
 			static Id()
 			{
@@ -164,16 +167,37 @@ namespace MimAcher
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int EscolherFoto = 2130903040;
+			public const int Competencias = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Inscrever = 2130903041;
+			public const int EscolherFoto = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int Main = 2130903042;
+			public const int Gostos = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int TelaOpcoes = 2130903043;
+			public const int Inscrever = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int Interesses = 2130903044;
+			
+			// aapt resource value: 0x7f030005
+			public const int Main = 2130903045;
+			
+			// aapt resource value: 0x7f030006
+			public const int OpcoesArte = 2130903046;
+			
+			// aapt resource value: 0x7f030007
+			public const int OpcoesDisciplinas = 2130903047;
+			
+			// aapt resource value: 0x7f030008
+			public const int OpcoesEsporte = 2130903048;
+			
+			// aapt resource value: 0x7f030009
+			public const int OpcoesMusica = 2130903049;
+			
+			// aapt resource value: 0x7f03000a
+			public const int Resultado = 2130903050;
 			
 			static Layout()
 			{
