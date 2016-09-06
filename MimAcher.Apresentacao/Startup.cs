015b42bbfc1d12bf27ea7541c5c4abc8dafb2e03
Phoenix Project <phoenixproject.erp@gmail.com>
2016-09-06 08:52:31 -0300

    Projeto de Aplicação, Apresentação e Webservice criados.

---
+++ MimAcher.Apresentacao/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MimAcher.Apresentacao.Startup))]
+namespace MimAcher.Apresentacao
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MimAcher.WebService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MimAcher.WebService.MvcApplication" Language="C#" %>
