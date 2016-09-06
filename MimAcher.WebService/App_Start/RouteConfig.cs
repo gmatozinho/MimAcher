c2ae886ba129e80cfe505a547580355b01860ad5
Phoenix Project <phoenixproject.erp@gmail.com>
2016-09-06 20:58:54 -0300

    Config Ajustes

--- MimAcher.WebService/App_Start/RouteConfig.cs
+++ MimAcher.WebService/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MimAcher.WebService
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
