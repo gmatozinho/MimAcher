73133233942ed61017ddf17ce2c2e96284683a4e
Phoenix Project <phoenixproject.erp@gmail.com>
2016-09-06 22:00:31 -0300

    Apresentacao reconfigurada

--- MimAcher.Apresentacao/Startup.cs
+++ MimAcher.Apresentacao/Startup.cs
@@ -6,7 +6,7 @@ namespace MimAcher.Apresentacao
 {
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
-            ConfigureAuth(app);
+            
         }
     }
 }
