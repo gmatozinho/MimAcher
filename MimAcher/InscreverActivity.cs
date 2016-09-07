51431cd9d4f0b46bd7b372e196d1a744da37b5e7
gmatozinho <gustavomatozinho@gmail.com>
2016-09-07 19:12:55 -0300

    Tela_2 finalizada

--- MimAcher/InscreverActivity.cs
+++ MimAcher/InscreverActivity.cs
@@ -21,11 +21,52 @@ namespace MimAcher
 
             // Create your application here
             SetContentView(Resource.Layout.Inscrever);
-            //Initializing button from layout
-            Button avançar = FindViewById<Button>(Resource.Id.avançar);
+            //Initializing button and variables from layout
+            String user = "Fulano";
+            String password = null;
+            String name = null;
+            String email = null;
+            String age = null;
+            String phone = null;
 
+            //Resgatando o que foi digitado nos EditText
+            Button avançar = FindViewById<Button>(Resource.Id.avançar);
+            EditText usuario = FindViewById<EditText>(Resource.Id.usuario);
+            EditText senha = FindViewById<EditText>(Resource.Id.senha);
+            EditText nome = FindViewById<EditText>(Resource.Id.nome);
+            EditText e_mail = FindViewById<EditText>(Resource.Id.email);
+            EditText idade = FindViewById<EditText>(Resource.Id.idade);
+            EditText telefone = FindViewById<EditText>(Resource.Id.telefone);
 
             //Pegar as informações inseridas
+            usuario.TextChanged += (object sender, Android.Text.TextChangedEventArgs u) => {
+                user = u.Text.ToString();
+            };
+
+            senha.TextChanged += (object sender, Android.Text.TextChangedEventArgs p) => {
+                password = p.Text.ToString();
+            };
+
+            nome.TextChanged += (object sender, Android.Text.TextChangedEventArgs n) => {
+                name = n.Text.ToString();
+            };
+
+            e_mail.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => {
+                email = e.Text.ToString();
+            };
+
+            idade.TextChanged += (object sender, Android.Text.TextChangedEventArgs a) => {
+                age = a.Text.ToString();
+            };
+
+            telefone.TextChanged += (object sender, Android.Text.TextChangedEventArgs p) => {
+                phone = p.Text.ToString();
+
+            };
+
+            //Inserir informações no banco, porém antes checar persistência
+            
+            
 
             //Choose Picture button click action
             avançar.Click += delegate {
