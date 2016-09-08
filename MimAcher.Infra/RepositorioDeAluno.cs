2f3f4e19b81af0154efd3e66025758009923e1b4
Phoenix Project <phoenixproject.erp@gmail.com>
2016-09-08 14:11:22 -0300

    Conexão com o banco criada e primeiro repositório criado.

---
+++ MimAcher.Infra/RepositorioDeAluno.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace MimAcher.Infra
+{
+    public class RepositorioDeAluno
+    {
+        public MIMACHEREntities Contexto { get; set; }
+
+        public RepositorioDeAluno()
+        {
+            this.Contexto = new MIMACHEREntities();
+        }
+
+        public List<ALUNO> ObterTodosOsAlunos()
+        {
+            return this.Contexto.ALUNO.ToList();
+        }
+
+        public List<ALUNO> ObterTodosOsAlunosPorNome(String nome)
+        {
+            return this.Contexto.ALUNO.Where(l => l.nome.Equals(nome)).ToList();            
+        }
+
+        public ALUNO ObterAlunoPorLogin(String login)
+        {
+            return this.Contexto.ALUNO.Where(l => l.login.Equals(login)).SingleOrDefault();
+        }
+
+        
+        public void InserirAluno(ALUNO Aluno)
+        {            
+            this.Contexto.ALUNO.Add(Aluno);
+            this.Contexto.SaveChanges();         
+        }
+
+        public int BuscarQuantidadeRegistros()
+        {
+            return this.Contexto.ALUNO.Count();
+        }
+                
+        public void RemoverAluno(ALUNO Aluno)
+        {
+            this.Contexto.ALUNO.Remove(Aluno);
+            this.Contexto.SaveChanges();
+        }
+
+        public void AtualizarAluno(ALUNO Aluno)
+        {            
+            this.Contexto.Entry(Aluno).State = EntityState.Modified;
+            this.Contexto.SaveChanges();            
+        }
+    }
+}
