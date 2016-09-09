7844c90f0444ee37cee5efb55752fb33d7117e28
Phoenix Project <phoenixproject.erp@gmail.com>
2016-09-09 11:26:24 -0300

    Aplicação update.

--- MimAcher.Aplicacao/GestorDeAluno.cs
+++ MimAcher.Aplicacao/GestorDeAluno.cs
@@ -9,5 +9,43 @@ namespace MimAcher.Aplicacao
 {
     public class GestorDeAluno
     {
+        public RepositorioDeAluno RepositorioDeAluno { get; set; }
+
+        public GestorDeAluno()
+        {
+            this.RepositorioDeAluno = new RepositorioDeAluno();
+        }
+
+        public List<ALUNO> ObterTodosOsAlunos()
+        {
+            return this.RepositorioDeAluno.ObterTodosOsAlunos();
+        }
+
+        public List<ALUNO> ObterTodosOsAlunosPorNome(String nome)
+        {
+            return this.RepositorioDeAluno.ObterTodosOsAlunosPorNome(nome);
+        }
+
+        public ALUNO ObterAlunoPorLogin(String login)
+        {
+            return this.RepositorioDeAluno.ObterAlunoPorLogin(login);
+        }
+
+
+        public void InserirAluno(ALUNO Aluno)
+        {
+            this.RepositorioDeAluno.InserirAluno(Aluno);
+        }
+
+        public int BuscarQuantidadeRegistros()
+        {
+            return this.RepositorioDeAluno.BuscarQuantidadeRegistros();
+        }
+
+        public void RemoverAluno(ALUNO Aluno)
+        {
+            return this.RepositorioDeAluno.RemoverAluno(Aluno);
+        }
+
+
     }
-}
