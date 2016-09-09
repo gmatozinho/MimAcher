d38ad0298307467ce986c5c8a6f80d5bf01f7ff0
Phoenix Project <phoenixproject.erp@gmail.com>
2016-09-08 22:28:06 -0300

    Camada de Aplicação criada e alguns gestores adicionados.

---
+++ MimAcher.Aplicacao/GestorDeAprender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MimAcher.Infra;
+
+namespace MimAcher.Aplicacao
+{
+    public class GestorDeAprender
+    {
+        public RepositorioDeAprender RepositorioDeAprender { get; set; }
+
+        public GestorDeAprender()
+        {
+            this.RepositorioDeAprender = new RepositorioDeAprender();
+        }
+
+        public List<APRENDER> ObterTodosOsRegistrosDoQueSePodeAprender()
+        {
+            return RepositorioDeAprender.ObterTodosOsRegistrosDoQueSePodeAprender();
+        }
+
+        public List<APRENDER> ObterTodosOsRegistrosDoQueSePodeAprenderPorNome(String nome)
+        {
+            return this.RepositorioDeAprender.ObterTodosOsRegistrosDoQueSePodeAprenderPorNome(nome);
+        }
+
+        public void InserirNovoAprendizado(APRENDER aprender)
+        {
+            this.InserirNovoAprendizado(aprender);
+        }
+
+        public int BuscarQuantidadeRegistros()
+        {
+            return this.RepositorioDeAprender.BuscarQuantidadeRegistros();
+        }
+
+        public void RemoverAprendizado(APRENDER aprender)
+        {
+            this.RepositorioDeAprender.RemoverAprendizado(aprender);
+        }
+
+        public void AtualizarAprendizado(APRENDER aprender)
+        {
+            this.RepositorioDeAprender.AtualizarAprendizado(aprender);
+        }
+    }
+}
---
+++ MimAcher.Aplicacao/GestorDeUsuario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MimAcher.Infra;
+
+
+namespace MimAcher.Aplicacao
+{
+    public class GestorDeUsuario
+    {
+        public RepositorioDeUsuario RepositorioDeUsuario { get; set; }
+
+        public GestorDeUsuario(){
+            this.RepositorioDeUsuario = new RepositorioDeUsuario();
+        }
+
+        public List<USUARIO> ObterTodosOsUsuarios()
+        {
+            return this.ObterTodosOsUsuarios();
+        }
+
+        public USUARIO ObterUsuarioPorLogin(String login)
+        {
+            return this.RepositorioDeUsuario.ObterUsuarioPorLogin(login);
+        }
+
+
+        public void InserirUsuario(USUARIO usuario)
+        {
+            this.RepositorioDeUsuario.InserirUsuario(usuario);
+        }
+
+        public int BuscarQuantidadeRegistros()
+        {
+            return this.RepositorioDeUsuario.BuscarQuantidadeRegistros();
+        }
+
+        public void RemoverUsuario(USUARIO usuario)
+        {
+            this.RepositorioDeUsuario.RemoverUsuario(usuario);
+        }
+
+        public void AtualizarUsuario(USUARIO usuario)
+        {
+            this.RepositorioDeUsuario.AtualizarUsuario(usuario);
+        }
+    }
+}
