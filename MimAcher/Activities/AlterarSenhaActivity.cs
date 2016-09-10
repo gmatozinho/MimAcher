c224f1271ec51e2e1a65a54a826edb0bd3a65086
gmatozinho <gustavomatozinho@gmail.com>
2016-09-10 20:21:34 -0300

    Telas finalizadas(resultado e editar perfil)

---
+++ MimAcher/Activities/AlterarSenhaActivity.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MimAcher.Entidades;
+
+namespace MimAcher.Activities
+{
+    [Activity(Label = "AlterarSenhaActivity", Theme = "@style/Theme.Splash")]
+    public class AlterarSenhaActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Bundle aluno_bundle = Intent.GetBundleExtra("aluno");
+            Aluno aluno = AlunoFactory.criarAluno(aluno_bundle);
+
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.AlterarSenha);
+
+            EditText nova_senha = FindViewById<EditText>(Resource.Id.nova_senha);
+            EditText repita_nova_senha = FindViewById<EditText>(Resource.Id.repita_nova_senha);
+            Button confirmar = FindViewById<Button>(Resource.Id.confirmar);
+        }
+    }
+}
