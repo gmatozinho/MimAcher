c943574544b052cf4dca9d4d9e0233b710f99f0f
Cayo Donatti <cayopdonatti@gmail.com>
2016-09-20 11:15:07 -0300

    Validador não é mais static

--- MimAcher/Entidades/Validador.cs
+++ MimAcher/Entidades/Validador.cs
@@ -9,10 +9,11 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Text.RegularExpressions;
 
 namespace MimAcher.Entidades
 {
-    public static class Validador
+    public class Validador
     {
         public Boolean ValidarEmail(String email)
         {
@@ -38,9 +39,10 @@ namespace MimAcher.Entidades
 
         public Boolean ValidarData(String data)
         {
+            DateTime saida;
             bool isValid = DateTime.TryParseExact(data, "dd/MM/yyyy",
-                                                  CultureInfo.InvariantCulture,
-                                                  DateTimeStyles.None, out dt);
+                                                  System.Globalization.CultureInfo.InvariantCulture,
+                                                  System.Globalization.DateTimeStyles.None, out saida);
             return isValid;
         }
 
