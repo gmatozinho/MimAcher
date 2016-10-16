7a8de2c2092d08e6db348fb18fa7bc56449b1db2
Phoenix Project <phoenixproject.erp@gmail.com>
2016-10-16 18:44:37 -0200

    Controller Participante Hobbie adicionado em camada de WebService.

---
+++ MimAcher.WebService/Controllers/ParticipanteHobbieController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MimAcher.Infra;
+using MimAcher.Aplicacao;
+using MimAcher.WebService.Models;
+
+namespace MimAcher.WebService.Controllers
+{
+    public class ParticipanteHobbieController : Controller
+    {
+        public GestorDeHobbieDeParticipante GestorDeHobbieDeParticipante { get; set; }
+
+        public ParticipanteHobbieController()
+        {
+            this.GestorDeHobbieDeParticipante = new GestorDeHobbieDeParticipante();
+        }
+
+        // GET: ParticipanteHobbie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            List<MA_PARTICIPANTE_HOBBIE> listaparticipantehobbieoriginal = GestorDeHobbieDeParticipante.ObterTodosOsRegistros();
+            List<ParticipanteHobbie> listaparticipantehobbie = new List<ParticipanteHobbie>();
+
+            foreach (MA_PARTICIPANTE_HOBBIE pe in listaparticipantehobbieoriginal)
+            {
+                ParticipanteHobbie participantehobbie = new ParticipanteHobbie();
+
+                participantehobbie.cod_p_hobbie = pe.cod_p_hobbie;
+                participantehobbie.cod_participante = pe.cod_participante;
+                participantehobbie.cod_item = pe.cod_item;
+
+                listaparticipantehobbie.Add(participantehobbie);
+            }
+
+            JsonResult jsonResult = Json(new
+            {
+                data = listaparticipantehobbie
+            }, JsonRequestBehavior.AllowGet);
+
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+
+        [HttpPost]
+        public ActionResult Add(List<ParticipanteHobbie> listaparticipantehobbie)
+        {
+            JsonResult jsonResult;
+
+            //Verifica se o registro é inválido e se sim, retorna com erro.
+            if (listaparticipantehobbie == null)
+            {
+                jsonResult = Json(new
+                {
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+            else
+            {
+                foreach (ParticipanteHobbie pe in listaparticipantehobbie)
+                {
+                    MA_PARTICIPANTE_HOBBIE participantehobbie = new MA_PARTICIPANTE_HOBBIE();
+                    participantehobbie.cod_participante = pe.cod_participante;
+                    participantehobbie.cod_item = pe.cod_item;
+                }
+
+                jsonResult = Json(new
+                {
+                    success = true
+                }, JsonRequestBehavior.AllowGet);
+
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+        }
+    }
+}
