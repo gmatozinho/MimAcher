d8325926c8e2955190e3a53e75fe962d23535225
Phoenix Project <phoenixproject.erp@gmail.com>
2016-10-16 18:41:17 -0200

    Controller Participante Ensinar adicionado em camada de WebService.

---
+++ MimAcher.WebService/Models/ParticipanteHobbie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MimAcher.WebService.Models
+{
+    public class ParticipanteHobbie
+    {
+        public int cod_p_hobbie { get; set; }
+        public int cod_participante { get; set; }
+        public int cod_item { get; set; }
+    }
+}
