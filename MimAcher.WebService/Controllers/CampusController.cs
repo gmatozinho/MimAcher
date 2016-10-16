1e920f3f070888980d04931e8373aeced987d27e
Phoenix Project <phoenixproject.erp@gmail.com>
2016-10-16 17:12:23 -0200

    Campus Controller feito na camada de Webservice.

---
+++ MimAcher.WebService/Controllers/CampusController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MimAcher.Infra;
+using MimAcher.Aplicacao;
+using MimAcher.WebService.Models;
+
+namespace MimAcher.WebService.Controllers
+{
+    public class CampusController : Controller
+    {
+        public GestorDeCampus GestorDeCampus { get; set; }
+
+        public CampusController()
+        {
+            this.GestorDeCampus = new GestorDeCampus();
+        }
+
+        // GET: Campus
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            List<MA_CAMPUS> listacampusoriginal = GestorDeCampus.ObterTodosOsCampus();
+            List<Campus> listacampus = new List<Campus>();
+
+            foreach (MA_CAMPUS nc in listacampusoriginal)
+            {
+                Campus campus = new Campus();
+
+                campus.cod_campus = nc.cod_campus;
+                campus.local = nc.local;
+
+                listacampus.Add(campus);
+            }
+
+            JsonResult jsonResult = Json(new
+            {
+                data = listacampus
+            }, JsonRequestBehavior.AllowGet);
+
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+
+        [HttpPost]
+        public ActionResult Add(List<Campus> listacampus)
+        {
+            JsonResult jsonResult;
+
+            //Verifica se o registro é inválido e se sim, retorna com erro.
+            if (listacampus == null)
+            {
+                jsonResult = Json(new
+                {
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+            else
+            {
+                foreach (Campus nc in listacampus)
+                {
+                    MA_CAMPUS campus = new MA_CAMPUS();
+                    campus.local = nc.local;                    
+                }
+
+                jsonResult = Json(new
+                {
+                    success = true
+                }, JsonRequestBehavior.AllowGet);
+
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+        }
+    }
+}
