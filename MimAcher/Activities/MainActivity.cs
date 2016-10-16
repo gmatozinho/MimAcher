3ddf043dfc61e0647d1754402b410daf5ae22b76
Gustavo <gustavomatozinho@gmail.com>
2016-10-16 21:46:10 -0200

    Ajustes

--- MimAcher/Activities/MainActivity.cs
+++ MimAcher/Activities/MainActivity.cs
@@ -4,28 +4,39 @@ using Android.Content;
 using Android.OS;
 using Android.Widget;
 using MimAcher.Entidades;
+using System;
+using Android.Views;
+using System.Threading.Tasks;
+using Android.Gms.Common;
+using Android.Gms.Common.Apis;
+using Android.Support.V7.App;
 
 namespace MimAcher.Activities
 {
     [Activity(Label = "MimAcher", Theme = "@style/Theme.Splash")]
     public class MainActivity : Activity
     {
+        
         //Variaveis globais
         //até comunicar com o banco informações hipotéticas
         private readonly string _campus = "Serra";
         private readonly string _senha = null;
-        private readonly string _nome = "Gasparzinho";
+        private string _nome = "Gustavo";
         private readonly string _email = null;
         private readonly string _nascimento = "09/10/1995";
         private readonly string _telefone = "00000000";
         private string _userEmail;
         private string _userPassword;
 
+
+        private static readonly TaskScheduler UiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
         //Metodos do controlador
         //Cria e controla a activity
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
 
             //Exibindo o layout .axml
             SetContentView(Resource.Layout.Main);
@@ -40,12 +51,15 @@ namespace MimAcher.Activities
             //Resgatando o que foi digitado nos EditText
             campoEmail.TextChanged += (sender, e) => _userEmail = e.Text.ToString();
             campoSenha.TextChanged += (sender, p) => _userPassword = p.Text.ToString();
+
             
             //Login button click action, e passando o _nome do usuário para próxima activity
-            entrar.Click += delegate {
+            entrar.Click += delegate
+            {
+                ValidaLogin(_userEmail, _userPassword);
                 var participante = new Participante(MontarUsuário());
                 var resultadoActivity = new Intent(this, typeof(ResultadoActivity));
-                resultadoActivity.PutExtra("member",participante.ParticipanteToBundle());
+                resultadoActivity.PutExtra("member", participante.ParticipanteToBundle());
                 StartActivity(resultadoActivity);
             };
 
@@ -56,6 +70,22 @@ namespace MimAcher.Activities
             inscrevase.Click += delegate {
                 StartActivity(typeof(InscreverActivity));
             };
+
+
+        }
+
+        
+
+
+        private static bool ValidaLogin(string email, string senha)
+        {
+            bool check;
+            if (true)
+            {
+                check = true;
+            }
+
+            return check;
         }
 
         //função temporaria
@@ -74,13 +104,27 @@ namespace MimAcher.Activities
 
             return informacoes;
         }
-        
+
+
+        public void OnClick(View v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnConnected(Bundle connectionHint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnConnectionFailed(ConnectionResult result)
+        {
+            throw new NotImplementedException();
+        }
     }
-
-    
-
-
-
-
 }
 
