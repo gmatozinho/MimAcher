12c9e8716c6b7b71c07a15a7993c4fd6c1af1b15
Phoenix Project <phoenixproject.erp@gmail.com>
2016-10-16 18:06:22 -0200

    Item Controller Adicionado.

---
+++ MimAcher.WebService/Controllers/ItemController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MimAcher.Infra;
+using MimAcher.Aplicacao;
+using MimAcher.WebService.Models;
+
+namespace MimAcher.WebService.Controllers
+{
+    public class ItemController : Controller
+    {
+        public GestorDeItem GestorDeItem { get; set; }
+
+        public ItemController()
+        {
+            this.GestorDeItem = new GestorDeItem();
+        }
+
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            List<MA_ITEM> listaitemoriginal = GestorDeItem.ObterTodosOsItem();
+            List<Item> listaitem = new List<Item>();
+
+            foreach (MA_ITEM it in listaitemoriginal)
+            {
+                Item item = new Item();
+
+                item.cod_item = it.cod_item;
+                item.nome = it.nome;
+
+                listaitem.Add(item);
+            }
+
+            JsonResult jsonResult = Json(new
+            {
+                data = listaitem
+            }, JsonRequestBehavior.AllowGet);
+
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+
+        [HttpPost]
+        public ActionResult Add(List<Item> listaitem)
+        {
+            JsonResult jsonResult;
+
+            //Verifica se o registro é inválido e se sim, retorna com erro.
+            if (listaitem == null)
+            {
+                jsonResult = Json(new
+                {
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+            else
+            {
+                foreach (Item it in listaitem)
+                {
+                    MA_ITEM item = new MA_ITEM();
+                    item.nome = it.nome;
+                }
+
+                jsonResult = Json(new
+                {
+                    success = true
+                }, JsonRequestBehavior.AllowGet);
+
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+        }
+    }
+}
