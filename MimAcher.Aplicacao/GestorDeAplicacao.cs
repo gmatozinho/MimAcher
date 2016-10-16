160444d7abd3c6f0be05c752325082da31eced46
Phoenix Project <phoenixproject.erp@gmail.com>
2016-10-16 17:02:17 -0200

    webservice NAC refeito.

---
+++ MimAcher.Aplicacao/GestorDeAplicacao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MimAcher.Aplicacao
+{
+    public class GestorDeAplicacao
+    {
+        public Boolean TratarCampoString(String campo)
+        {
+
+            if (campo != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
