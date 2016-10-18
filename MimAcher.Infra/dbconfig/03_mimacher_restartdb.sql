c66de21d6bc8fc9537cd460352388c2141e9e0f8
Phoenix Project <phoenixproject.erp@gmail.com>
2016-10-18 21:50:54 -0200

    Alterado o script de limpeza de tabelas.

--- MimAcher.Infra/dbconfig/03_mimacher_restartdb.sql
+++ MimAcher.Infra/dbconfig/03_mimacher_restartdb.sql
@@ -1,21 +1,25 @@
-﻿delete from ma_aluno_gosto
-DBCC CHECKIDENT('ma_aluno_gosto', RESEED, 0)
-delete from ma_aluno_ensinar
-DBCC CHECKIDENT('ma_aluno_ensinar', RESEED, 0)
-delete from ma_aluno_aprender
-DBCC CHECKIDENT('ma_aluno_aprender', RESEED, 0)
-delete from ma_gosto
-DBCC CHECKIDENT('ma_gosto', RESEED, 0)
-delete from ma_ensinar
-DBCC CHECKIDENT('ma_ensinar', RESEED, 0)
-delete from ma_aprender
-DBCC CHECKIDENT('ma_aprender', RESEED, 0)
+﻿delete from ma_participante_ensinar
+DBCC CHECKIDENT('ma_participante_ensinar', RESEED, 0)
+delete from ma_participante_hobbie
+DBCC CHECKIDENT('ma_participante_hobbie', RESEED, 0)
+delete from ma_participante_aprender
+DBCC CHECKIDENT('ma_participante_aprender', RESEED, 0)
+delete from ma_item
+DBCC CHECKIDENT('ma_item', RESEED, 0)
+delete from ma_imagem_participante
+DBCC CHECKIDENT('ma_imagem_participante', RESEED, 0)
+delete from ma_participante
+DBCC CHECKIDENT('ma_participante', RESEED, 0)
 delete from ma_imagem_usuario
 DBCC CHECKIDENT('ma_imagem_usuario', RESEED, 0)
-delete from ma_aluno
-DBCC CHECKIDENT('ma_aluno', RESEED, 0)
-delete from ma_nac_campus
-DBCC CHECKIDENT('ma_nac_campus', RESEED, 0)
-delete from ma_usuario
+delete from ma_nac_area_atuacao
+DBCC CHECKIDENT('ma_nac_area_atuacao', RESEED, 0)
+delete from ma_area_atuacao
+DBCC CHECKIDENT('ma_area_atuacao', RESEED, 0)
+delete from ma_nac
 DBCC CHECKIDENT('ma_usuario', RESEED, 0)
+delete from ma_nac
+DBCC CHECKIDENT('ma_usuario', RESEED, 0)
+delete from ma_campus
+DBCC CHECKIDENT('ma_campus', RESEED, 0)
 
