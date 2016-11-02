c1b37feb89a8aa628e848cc8f2cd7c2eb1360f3d
Phoenix Project <phoenixproject.erp@gmail.com>
2016-11-02 09:30:43 -0200

    Referência para novo modelo de Domínio criado no webform de Login.

--- MimAcher.Apresentacao/Login.aspx.cs
+++ MimAcher.Apresentacao/Login.aspx.cs
@@ -6,7 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Ext.Net;
 using MimAcher.Aplicacao;
-using MimAcher.Infra;
+using MimAcher.Dominio;
 
 namespace MimAcher.Apresentacao
 {
