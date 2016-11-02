7fb5e48153d42d6174d1281f5190d947cc1e1a4b
Phoenix Project <phoenixproject.erp@gmail.com>
2016-11-02 08:25:43 -0200

    Método Update de controller de NAC e Área de Atuação criado.

--- MimAcher.WebService/Controllers/NACAreaAtuacaoController.cs
+++ MimAcher.WebService/Controllers/NACAreaAtuacaoController.cs
@@ -87,5 +87,43 @@ namespace MimAcher.WebService.Controllers
                 return jsonResult;
             }
         }
+
+        [HttpPost]
+        public ActionResult Update(List<NACAreaAtuacao> listanacareaatuacao)
+        {
+            JsonResult jsonResult;
+
+            //Verifica se o registro é inválido e se sim, retorna com erro.
+            if (listanacareaatuacao == null)
+            {
+                jsonResult = Json(new
+                {
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+            else
+            {
+                foreach (NACAreaAtuacao na in listanacareaatuacao)
+                {
+                    MA_NAC_AREA_ATUACAO nacareaatuacao = new MA_NAC_AREA_ATUACAO();
+                    nacareaatuacao.cod_nac_area_atuacao = na.cod_nac_area_atuacao;
+                    nacareaatuacao.cod_area_atuacao = na.cod_area_atuacao;
+                    nacareaatuacao.cod_nac = na.cod_nac;
+
+                    GestorDeNACAreaDeAtuacao.InserirNACAreaDeAtuacao(nacareaatuacao);
+                }
+
+                jsonResult = Json(new
+                {
+                    success = true
+                }, JsonRequestBehavior.AllowGet);
+
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+        }
     }
 }
