00acd3b6956f66f5e3dc292ba2695ad430e5babb
Phoenix Project <phoenixproject.erp@gmail.com>
2016-11-02 09:27:04 -0200

    Referência de camada de Domínio em controller de NAC Área de Atuação na camada de Webservice, criada.

--- MimAcher.WebService/Controllers/NACAreaAtuacaoController.cs
+++ MimAcher.WebService/Controllers/NACAreaAtuacaoController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MimAcher.Infra;
+using MimAcher.Dominio;
 using MimAcher.Aplicacao;
 using MimAcher.WebService.Models;
 
