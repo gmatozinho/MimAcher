5af47179725e732fef5a6d8c6fa5d27910202c3f
Phoenix Project <phoenixproject.erp@gmail.com>
2016-11-02 09:26:42 -0200

    Referência de camada de Domínio em controller de NAC camada de Webservice, criada.

--- MimAcher.WebService/Controllers/NACController.cs
+++ MimAcher.WebService/Controllers/NACController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MimAcher.Aplicacao;
-using MimAcher.Infra;
+using MimAcher.Dominio;
 using MimAcher.WebService.Models;
 
 namespace MimAcher.WebService.Controllers
