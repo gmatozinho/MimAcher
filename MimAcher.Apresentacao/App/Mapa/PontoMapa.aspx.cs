e32ec28bf2fda1bcea2634f9b9f7a2348f5a3b8a
Phoenix Project <phoenixproject.erp@gmail.com>
2016-11-02 13:05:08 -0200

    Adicionado mapa (sem chamada) para exibir localização do Participante.

---
+++ MimAcher.Apresentacao/App/Mapa/PontoMapa.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MimAcher.Aplicacao;
+using MimAcher.Dominio;
+using Subgurim.Controles;
+using Subgurim.Controles.GoogleChartIconMaker;
+using System.Drawing;
+
+namespace MimAcher.Apresentacao.App.Mapa
+{
+    public partial class PontoMapa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ExibirTodosOsPontosNoMapa()
+        {
+            MA_PARTICIPANTE participante = (MA_PARTICIPANTE)Session["participante"];
+                        
+            GLatLng mainLocation = new GLatLng(Convert.ToDouble(participante.geolocalizacao.Latitude), Convert.ToDouble(participante.geolocalizacao.Longitude));
+            this.GMap1.setCenter(mainLocation, 15);
+
+            XPinLetter xpinLetter = new XPinLetter(PinShapes.pin_star, "H", Color.Blue, Color.White, Color.Chocolate);
+            this.GMap1.Add(new GMarker(mainLocation, new GMarkerOptions(new GIcon(xpinLetter.ToString(), xpinLetter.Shadow()))));
+
+        }
+    }
+}
