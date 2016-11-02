34d62e3ae5ae6f4b4a7bbc380abe92ef104d60e0
Phoenix Project <phoenixproject.erp@gmail.com>
2016-11-02 09:21:04 -0200

    Referência de camada de Domínio em Webform de Participante Ensinar na camada de Apresentação criada.

--- MimAcher.Apresentacao/App/ParticipanteEnsinar.aspx.cs
+++ MimAcher.Apresentacao/App/ParticipanteEnsinar.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using MimAcher.Infra;
+using MimAcher.Dominio;
 using MimAcher.Aplicacao;
 using Ext.Net;
 
