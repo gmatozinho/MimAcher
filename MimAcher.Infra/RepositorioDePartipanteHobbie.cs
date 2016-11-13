090bd2709dc9f247211ebbd8f1556c8b5eac7af1
Phoenix Project <phoenixproject.erp@gmail.com>
2016-11-13 10:15:50 -0200

    Readição do prefixo this em Repositório de Participante/Hobbie para referenciar corretamente atributo de classe.

--- MimAcher.Infra/RepositorioDePartipanteHobbie.cs
+++ MimAcher.Infra/RepositorioDePartipanteHobbie.cs
@@ -12,42 +12,42 @@ namespace MimAcher.Infra
 
         public RepositorioDePartipanteHobbie()
         {
-            Contexto = new MIMACHEREntities();
+            this.Contexto = new MIMACHEREntities();
         }
 
         public MA_PARTICIPANTE_HOBBIE ObterHobbieDoParticipantePorId(int id)
         {
-            return Contexto.MA_PARTICIPANTE_HOBBIE.Find(id);
+            return this.Contexto.MA_PARTICIPANTE_HOBBIE.Find(id);
         }
 
         public MA_PARTICIPANTE_HOBBIE ObterParticipanteHobbiePorItemEParticipante(MA_PARTICIPANTE_HOBBIE participantehobbie)
         {
-            return Contexto.MA_PARTICIPANTE_HOBBIE.Where(l => l.cod_participante == participantehobbie.cod_participante && l.cod_item == participantehobbie.cod_item).SingleOrDefault();
+            return this.Contexto.MA_PARTICIPANTE_HOBBIE.Where(l => l.cod_participante == participantehobbie.cod_participante && l.cod_item == participantehobbie.cod_item).SingleOrDefault();
         }
 
         public List<MA_PARTICIPANTE_HOBBIE> ObterTodosOsRegistros()
         {
-            return Contexto.MA_PARTICIPANTE_HOBBIE.ToList();
+            return this.Contexto.MA_PARTICIPANTE_HOBBIE.ToList();
         }
                 
         public void InserirNovoParticipanteHobbie(MA_PARTICIPANTE_HOBBIE hobbieparticipante)
         {
             if (!VerificarSeExisteRelacaoDeParticipanteAprender(hobbieparticipante))
             {
-                Contexto.MA_PARTICIPANTE_HOBBIE.Add(hobbieparticipante);
-                Contexto.SaveChanges();
+                this.Contexto.MA_PARTICIPANTE_HOBBIE.Add(hobbieparticipante);
+                this.Contexto.SaveChanges();
             }
         }
 
         public int BuscarQuantidadeRegistros()
         {
-            return Contexto.MA_PARTICIPANTE_HOBBIE.Count();
+            return this.Contexto.MA_PARTICIPANTE_HOBBIE.Count();
         }
 
         public void RemoverHobbieDoParticipante(MA_PARTICIPANTE_HOBBIE hobbiedoparticipante)
         {
-            Contexto.MA_PARTICIPANTE_HOBBIE.Remove(hobbiedoparticipante);
-            Contexto.SaveChanges();
+            this.Contexto.MA_PARTICIPANTE_HOBBIE.Remove(hobbiedoparticipante);
+            this.Contexto.SaveChanges();
         }
 
         public void AtualizarHobbieDoParticipante(MA_PARTICIPANTE_HOBBIE hobbieparticipante)
@@ -71,8 +71,8 @@ namespace MimAcher.Infra
         {
             MA_PARTICIPANTE_HOBBIE participantehobbie = new MA_PARTICIPANTE_HOBBIE();
 
-            Contexto.Entry(hobbieparticipante).State = EntityState.Modified;
-            Contexto.SaveChanges();
+            this.Contexto.Entry(hobbieparticipante).State = EntityState.Modified;
+            this.Contexto.SaveChanges();
         }
 
         public Boolean VerificarSeExisteRelacaoDeParticipanteAprender(MA_PARTICIPANTE_HOBBIE participantehobbie)
