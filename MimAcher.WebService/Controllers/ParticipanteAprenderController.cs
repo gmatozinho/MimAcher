753a87178b703429f9dc773aa6d993cb9a1991bd
Phoenix Project <phoenixproject.erp@gmail.com>
2016-12-05 00:50:53 -0200

    Inserção de tratamento de erro em método Delete de controller Participante/Aprender em camada de webservice.

--- MimAcher.WebService/Controllers/ParticipanteAprenderController.cs
+++ MimAcher.WebService/Controllers/ParticipanteAprenderController.cs
@@ -191,27 +191,49 @@ namespace MimAcher.WebService.Controllers
             {
                 if (this.GestorDeParticipanteAprender.VerificarSeExisteRelacaoUsuarioAprenderPorIdDaRelacao(listaparticipanteaprender[0].cod_p_aprender))
                 {
-                    MA_PARTICIPANTE_APRENDER participanteaprender = this.GestorDeParticipanteAprender.ObterAprendizadoDoParticipantePorId(listaparticipanteaprender[0].cod_p_aprender);
-
-                    if (participanteaprender.cod_s_relacao == 1)
+                    try
                     {
-                        participanteaprender.cod_p_aprender = listaparticipanteaprender[0].cod_p_aprender;
-                        participanteaprender.cod_participante = listaparticipanteaprender[0].cod_participante;
-                        participanteaprender.cod_item = listaparticipanteaprender[0].cod_item;
-                        //Marca a relação como inativa
-                        participanteaprender.cod_s_relacao = 2;
+                        MA_PARTICIPANTE_APRENDER participanteaprender = this.GestorDeParticipanteAprender.ObterAprendizadoDoParticipantePorId(listaparticipanteaprender[0].cod_p_aprender);
 
-                        this.GestorDeParticipanteAprender.AtualizarAprendizadoDeParticipante(participanteaprender);
-
-                        jsonResult = Json(new
+                        if (participanteaprender.cod_s_relacao == 1)
                         {
-                            codigo = participanteaprender.cod_p_aprender
-                        }, JsonRequestBehavior.AllowGet);
+                            participanteaprender.cod_p_aprender = listaparticipanteaprender[0].cod_p_aprender;
+                            participanteaprender.cod_participante = listaparticipanteaprender[0].cod_participante;
+                            participanteaprender.cod_item = listaparticipanteaprender[0].cod_item;
+                            //Marca a relação como inativa
+                            participanteaprender.cod_s_relacao = 2;
+
+                            try
+                            {
+                                this.GestorDeParticipanteAprender.AtualizarAprendizadoDeParticipante(participanteaprender);
+
+                                jsonResult = Json(new
+                                {
+                                    codigo = participanteaprender.cod_p_aprender
+                                }, JsonRequestBehavior.AllowGet);
+                            }
+                            catch(Exception e)
+                            {
+                                jsonResult = Json(new
+                                {
+                                    erro = e.InnerException.ToString(),
+                                    codigo = -1
+                                }, JsonRequestBehavior.AllowGet);
+                            }
+                        }
+                        else
+                        {
+                            jsonResult = Json(new
+                            {
+                                codigo = -1
+                            }, JsonRequestBehavior.AllowGet);
+                        }
                     }
-                    else
+                    catch(Exception e)
                     {
                         jsonResult = Json(new
                         {
+                            erro = e.InnerException.ToString(),
                             codigo = -1
                         }, JsonRequestBehavior.AllowGet);
                     }
